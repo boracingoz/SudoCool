0aa80767cea70dcc3a915f6211289592afabbb29
boracingoz26 <boracingoz@gmail.com>
2025-05-16 19:30:34 +0300

    Button UI and particle effects added.

---
+++ Assets/Scripts/FloatMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FloatMover : MonoBehaviour
+{
+    [Header("Move Settings")]
+    public float amplitude = 1f;      
+    public float speed = 1f;
+    public bool randomizePhase = true;
+
+    private Vector3 startPos;
+    private Vector3 axis;
+    private float phase;
+
+    void Awake()
+    {
+        
+        startPos = transform.position;
+
+        axis = Random.onUnitSphere;
+        axis.z = 0; 
+
+        phase = randomizePhase ? Random.Range(0f, Mathf.PI * 2f) : 0f;
+    }
+
+    void Update()
+    {
+        float t = Time.time * speed + phase;
+
+        Vector3 offset = axis * (Mathf.Sin(t) * amplitude);
+
+        transform.position = startPos + offset;
+    }
+}
